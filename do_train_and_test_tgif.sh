4635a3ea70da7b04f186b01bb5cbee45704aa117
fangmingzhou <842926716@qq.com>
2021-03-28 20:28:00 +0800

    add tgif script

---
+++ do_train_and_test_tgif.sh
@@ -0,0 +1,72 @@
+# rootpath=$HOME/VisualSearch
+rootpath=data
+overwrite=0
+trainCollection=tgiftrain
+valCollection=tgifval
+testCollection=tgiftest
+
+
+case $1 in
+11)
+    config=w2vvpp_resnext101-resnet152_subspace_bow_w2v
+    ;;
+12)
+    config=w2vvpp_resnext101-resnet152_multispace_bow_w2v
+    ;;
+21)
+    config=w2vvpp_resnext101-resnet152_subspace_bow_w2v_gru
+    ;;
+22)
+    config=w2vvpp_resnext101-resnet152_multispace_bow_w2v_gru
+    ;;
+31)
+    config=w2vvpp_resnext101-resnet152_subspace_bow_w2v_bigru
+    ;;
+32)
+    config=w2vvpp_resnext101-resnet152_multispace_bow_w2v_bigru
+    ;;
+41)
+    config=w2vvpp_resnext101-resnet152_subspace_bow_w2v_bert
+    ;;
+42)
+    config=w2vvpp_resnext101-resnet152_multispace_bow_w2v_bert
+    ;;
+51)
+    config=w2vvpp_resnext101-resnet152_subspace_bow_w2v_gru_bert
+    ;;
+52)
+    config=w2vvpp_resnext101-resnet152_multispace_bow_w2v_gru_bert
+    ;;
+61)
+    config=w2vvpp_resnext101-resnet152_subspace_bow_w2v_bigru_bert
+    ;;
+62)
+    config=w2vvpp_resnext101-resnet152_multispace_bow_w2v_bigru_bert
+    ;;
+
+*)
+    config=-1
+    ;;
+esac
+
+gpu=$2
+prefix=runs_$3
+
+# ---train---
+CUDA_VISIBLE_DEVICES=$gpu python trainer.py $trainCollection $valCollection --overwrite $overwrite --rootpath $rootpath --config $config --model_prefix $prefix
+#exit
+
+# ---test---
+model_path=$rootpath/$trainCollection/sea_train/$valCollection/$config/$prefix/model_best.pth.tar
+sim_name=$trainCollection/sea_train/$valCollection/$config/$prefix
+
+if [ ! -f "$model_path" ]; then
+    echo "model not found: $model_path"
+    exit
+fi
+
+CUDA_VISIBLE_DEVICES=$gpu python predictor.py $testCollection $model_path $sim_name \
+    --query_sets $testCollection.caption.txt \
+    --rootpath $rootpath --overwrite $overwrite
+
+
